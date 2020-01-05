02d4fbcb0231cc6c4d03390d1e6a7bcd468686e0
Lucas Krul <l3k.scorp@gmail.com>
2020-01-05 01:25:17 -0300

    CSS da tela principal

--- src/models/Login.php
+++ src/models/Login.php
@@ -1,7 +1,23 @@
 <?php
 class Login extends Model {
+    public function validate() {
+        $errors = [];
+
+        if(!$this->email) {
+            $errors['email'] = 'E-mail é um campo obrigatório.';
+        }
+        
+        if(!$this->password) {
+            $errors['password'] = 'Por favor, informe a senha.';
+        }
+
+        if(count($errors) > 0){
+            throw new ValidationException($errors);
+        }
+    }
 
     public function checkLogin() {
+        $this->validate();
         $user = User::getOne(['email' => $this->email]);
         if($user) {
             if($user->end_date) {
